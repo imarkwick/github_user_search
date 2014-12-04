2ae2ec5c3d93e80a2a2543f4843017467bade9d6
imarkwick <izzymarkwick@gmail.com>
2014-12-04 14:34:37 +0000

    search result printing

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -1,12 +1,17 @@
 githubUserSearch.controller('GitUserSearchController', function($scope, $resource) {
 
   var searchResource = $resource('https://api.github.com/search/users');
-  
+  var searchMessage = 'You searched for: '
+
   $scope.doSearch = function() {
-    $scope.searchResult = searchResource.get({
-      q: $scope.searchTerm,
-      client_id: "d6dc6a59ba1cebdb9205",
-      client_secret: 'eb1e63221b5d03abd382de7075d5622ddb94e2c0'
-    });
+  	$('#search_for').hide()
+  	if ($scope.searchTerm != '') {
+	    $scope.searchResult = searchResource.get({
+	      q: $scope.searchTerm,
+	      client_id: "d6dc6a59ba1cebdb9205",
+	      client_secret: 'eb1e63221b5d03abd382de7075d5622ddb94e2c0'
+	    });
+	    $('#search_for').show()  	
+	  }
   };
 });
