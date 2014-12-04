a84f174862de81ae80c3f6324729bcae9a8c7732
ed <ejbyne@gmail.com>
2014-12-04 21:20:05 +0000

    now working and finding commits for bookmark manager

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -1,17 +1,40 @@
 githubUserSearch.controller('GitUserSearchController', function($scope, $resource) {
 
   var searchResource = $resource('https://api.github.com/search/users');
-  var searchMessage = 'You searched for: '
+  var repoResource = $resource('https://api.github.com/repos/makersacademy/bookmark_manager/commits');
+  var creds = {
+      client_id: "d6dc6a59ba1cebdb9205",
+      client_secret: 'eb1e63221b5d03abd382de7075d5622ddb94e2c0'
+    };
 
   $scope.doSearch = function() {
   	$('#search_for').hide()
+
   	if ($scope.searchTerm != '') {
 	    $scope.searchResult = searchResource.get({
 	      q: $scope.searchTerm,
 	      client_id: "d6dc6a59ba1cebdb9205",
 	      client_secret: 'eb1e63221b5d03abd382de7075d5622ddb94e2c0'
 	    });
-	    $('#search_for').show()  	
-	  }
-  };
+
+      var searchRepo = repoResource.query(creds)
+
+      var setCommitCount = function() {
+        searchRepo.$promise.then(function(result) {
+          $scope.repoCommitCount = result.length;
+          var commits = 0;
+          for (var i = 0; i < result.length; i++) {
+            if (result[i].committer.login === $scope.searchTerm) {
+              var commits = commits + 1
+            }
+          }
+          $scope.userCommitCount = commits;
+        })
+      }
+    
+      setCommitCount();
+      $('#search_for').show();
+      $('#repos').show();
+    }
+  }
 });
