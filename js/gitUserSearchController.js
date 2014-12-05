fe070c872becff9c1ee1c3ad575c06158aee462f
ed <ejbyne@gmail.com>
2014-12-05 08:54:59 +0000

    added search for all makers repos

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -1,40 +1,63 @@
 githubUserSearch.controller('GitUserSearchController', function($scope, $resource) {
 
   var searchResource = $resource('https://api.github.com/search/users');
-  var repoResource = $resource('https://api.github.com/repos/makersacademy/bookmark_manager/commits');
+  var repoResource = $resource('https://api.github.com/orgs/makersacademy/repos'); 
+  // /bookmark_manager/commits');
   var creds = {
       client_id: "d6dc6a59ba1cebdb9205",
-      client_secret: 'eb1e63221b5d03abd382de7075d5622ddb94e2c0'
+      client_secret: 'eb1e63221b5d03abd382de7075d5622ddb94e2c0',
+      per_page: '100'
     };
 
   $scope.doSearch = function() {
-  	$('#search_for').hide()
+    $('#search_for').hide()
 
-  	if ($scope.searchTerm != '') {
-	    $scope.searchResult = searchResource.get({
-	      q: $scope.searchTerm,
-	      client_id: "d6dc6a59ba1cebdb9205",
-	      client_secret: 'eb1e63221b5d03abd382de7075d5622ddb94e2c0'
-	    });
+    if ($scope.searchTerm != '') {
+      $scope.searchResult = searchResource.get({
+        q: $scope.searchTerm,
+        client_id: "d6dc6a59ba1cebdb9205",
+        client_secret: 'eb1e63221b5d03abd382de7075d5622ddb94e2c0',
+        per_page: '100'
+      });
 
-      var searchRepo = repoResource.query(creds)
+      var repos = [];
+      var commits = 0;
 
       var setCommitCount = function() {
-        searchRepo.$promise.then(function(result) {
-          $scope.repoCommitCount = result.length;
-          var commits = 0;
-          for (var i = 0; i < result.length; i++) {
-            if (result[i].committer.login === $scope.searchTerm) {
-              var commits = commits + 1
-            }
-          }
-          $scope.userCommitCount = commits;
+        repoResource.query(creds, function(result) {
+          result.forEach(function(repo) {
+            repos.push({ name: repo.name });
+          })
+          repos.forEach(function(item) {
+            var selectedRepo = $resource('https://api.github.com/repos/makersacademy/' + item.name + '/commits');
+            selectedRepo.query(creds, function(result) {
+              item.totalCommits = result.length;
+              item.userCommits = 0;
+              result.forEach(function(commit) {
+                if (commit.committer.login === $scope.searchTerm) {
+                  item.userCommits = item.userCommits + 1;
+                }
+              })
+            })
+          })
+          $scope.repos = repos;
         })
       }
-    
+
       setCommitCount();
       $('#search_for').show();
       $('#repos').show();
     }
   }
 });
+
+          // }
+          // $scope.repoList = repos;
+          // $scope.repoCommitCount = result.length;
+          // var commits = 0;
+          // for (var i = 0; i < result.length; i++) {
+          //   if (result[i].committer.login === $scope.searchTerm) {
+          //     var commits = commits + 1
+          //   }
+          // }
+          // $scope.userCommitCount = commits;
